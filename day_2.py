b036e62989da194af5b0e519e94d2570fd9de765
lily047 <meghanaballi07@gmail.com>
2026-01-21 17:01:22 +0530

    created a function to convert csv to json

--- day_2.py
+++ day_2.py
@@ -1,3 +1,6 @@
+import csv 
+import json 
+
 class EmailValidator(): 
 
     def __init__(self, email):
@@ -39,12 +42,45 @@ class EmailValidator():
                 flag = True 
 
         return domain 
+    
+    def get_domain_2(self): 
+
+        domain = self.email.split('@')[1]
+
+        return domain 
+    
+    def get_domain_3(self): 
+
+        domain = self.email.rstrip("@")
+
+        return domain
+
+def read_csv_convert_json(filename): 
+
+    
+
+    with open(filename, 'r') as f: 
+
+        reader = csv.DictReader(f)
+
+        rows = list(reader)
+
+    with open('student_data.json', 'w') as output_file: 
+
+        json.dump(rows, output_file, indent=2)
+
+
+
         
 def main(): 
 
     e=EmailValidator('Meghana@gmail.com')
     print(e.vaildate())
     print(e.get_domain())
+    print(e.get_domain_2())
+    print(e.get_domain_3())
+
+    read_csv_convert_json('data.csv')
 
 if __name__ == '__main__': 
 
